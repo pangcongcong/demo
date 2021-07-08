f4121e6997d43b04c67b96781d47c29a195e7ea2
ccpang <pangcongcong@users.noreply.github.com>
2021-07-08 14:19:11 +0800

    Create getPhoneNumber.vue

---
+++ getPhoneNumber.vue
@@ -0,0 +1,23 @@
+<template>
+  <button hover-class="none" open-type="getAuthorize" scope="phoneNumber" @error="onError" @getAuthorize="onGetAuthorize">获取手机号</button>
+</template>
+
+
+<script lang="ts">
+export default {
+  
+  data() {
+    return {
+      
+    };
+  },
+  methods: {
+    onError(e) {
+      console.log('e: ', e)
+    },
+     onGetAuthorize() {
+      console.log('onGetAuthorize res: ')
+    },
+  },
+};
+</script>
