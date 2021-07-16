4cb7e19946e5540dd3dcf7a142a272cf0c0eee2b
ccpang <pangcongcong@users.noreply.github.com>
2021-07-16 17:09:55 +0800

    Create searchInput.vue

---
+++ searchInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <input :class="[searchFixed ? 'class-fixed' : '']" />
+</template>
+
+<style>
+.class-fixed {
+  position: fixed;
+  top: 0;
+}
+</style>
+
+<script lang="ts">
+export default {
+  props: {
+    searchFixed: {
+      type: Boolean
+    }
+  },
+  
+  data() {
+    return {
+      
+    };
+  },
+  methods: {
+  
+  },
+};
+</script>
