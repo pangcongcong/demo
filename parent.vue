3c150083bcf7e811a2615f29837d10131aca54d5
ccpang <pangcongcong@users.noreply.github.com>
2021-07-16 17:04:43 +0800

    Create parent.vue

---
+++ parent.vue
@@ -0,0 +1,27 @@
+<template>
+  <searchInput :searchFixed="searchFixed"></searchInput>
+</template>
+
+
+<script lang="ts">
+import searchInput from './searchInput.vue'
+export default {
+  components: {
+    searchInput
+  },
+  
+  data() {
+    return {
+      searchFixed: false
+    };
+  },
+  onPageScroll(e){
+    if (e.scrollTop > 300 ) {
+      this.searchFixed = true
+    }
+  },
+  methods: {
+  
+  },
+};
+</script>
