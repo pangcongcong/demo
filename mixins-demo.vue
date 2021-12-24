5b4752ce78ee67ebcf62e9c566137fd58f29ea8d
ccpang <pangcongcong@users.noreply.github.com>
2021-12-24 10:55:38 +0800

    mixins

---
+++ mixins-demo.vue
@@ -0,0 +1,11 @@
+<template>
+  <view class"test">test</view>
+</template>
+
+<script lang="ts">
+  import mfPageMixin from '@/services/app/page'
+  
+  export default {
+    mixins: [mfPageMixin]
+  }
+</script>
